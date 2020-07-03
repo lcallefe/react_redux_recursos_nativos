041079700a85e5cce54d6a26f56b71f68bd8eaed
lcallefe <luciana_donadio@hotmail.com>
2020-07-03 20:31:25 -0300

    primeira parte recursos nativos react

---
+++ telas/MapaTela.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, Stylesheet } from 'react-native';
+
+const MapaTela = (props) => {
+    return (
+        <View>
+            {MapaTela}
+        </View>
+    )
+};
+
+const estilos = Stylesheet.create ({
+
+
+});
+
+export default MapaTela;
